849eb9288921d02caeee82436b6c7db7f0de5599
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-02-11 16:30:25 +0800

    push last changes before continuing this app

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,19 +4,26 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center items-center">
-                <div class="flex flex-col">
-                    <div class=" p-4 text-gray-900 dark:text-gray-100 text-right" style="font-size: 1.5rem; line-height:1rem; color:yellow;">
-                        {{ __("The Number 1") }}
+
+                <div class="flex flex-col gap-2">
+                    <div class="p-4 text-gray-900 dark:text-gray-100 text-right" style="font-size: 2.5rem; line-height:2rem; color:yellow;">{{ __("Ramz Bakeshoppe") }}
                     </div>
-                    <div class="text-center p-4 text-gray-900 dark:text-gray-100" style="font-size: 2.5rem; line-height:2rem">
-                        {{ __("Bread nang mga Pilipino!") }}
+                    <div class="px-4 pb-4 dark:text-gray-100 text-right" style="font-size: 2.5rem; line-height:2rem;">{{ __("The #1 Choice for  Filipino Bread!") }}</div>
+                    <div class="text-right p-4 text-gray-900 dark:text-gray-100" style="font-size: 1.5rem; line-height:2.25rem">
+                        {{ __("Freshly Baked Goodness, Loved by Filipinos. Franchise Now or Order Online!") }}
+                    </div>
+                    <div class="p-4 flex gap-4"> 
+                        <x-primary-button>{{ __('Franchise Now') }}</x-primary-button>
+                        <x-secondary-button>{{ __('Order Online') }}</x-primary-button>
                     </div>
                 </div>
+
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <img src="{{ asset('images/bread.png') }}" alt="Your Logo" class="h-24 w-auto">
                     </a>
                 </div>
+
             </div>
         </div>
     </div>
