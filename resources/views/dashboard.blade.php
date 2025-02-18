3981f84f1edc2e15b07a3bf148ddaba751eb97f2
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-02-18 08:37:29 +0800

    discover lightmode/darkmode boy

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,10 @@
             <div class="flex justify-center items-center">
 
                 <div class="flex flex-col gap-2">
-                    <div class="p-4 text-gray-900 dark:text-gray-100 text-right" style="font-size: 2.5rem; line-height:2rem; color:yellow;">{{ __("Ramz Bakeshoppe") }}
+                    <div class="p-4 text-gray-900 dark:text-gray-100 text-left" style="font-size: 2.5rem; line-height:2rem; color:yellow;">{{ __("Ramz Bakeshoppe") }}
                     </div>
-                    <div class="px-4 pb-4 dark:text-gray-100 text-right" style="font-size: 2.5rem; line-height:2rem;">{{ __("The #1 Choice for  Filipino Bread!") }}</div>
-                    <div class="text-right p-4 text-gray-900 dark:text-gray-100" style="font-size: 1.5rem; line-height:2.25rem">
+                    <div class="px-4 pb-4 dark:text-gray-100 text-left" style="font-size: 2.5rem; line-height:2rem;">{{ __("The #1 Choice for  Filipino Bread!") }}</div>
+                    <div class="text-left p-4 text-gray-900 dark:text-gray-100" style="font-size: 1.5rem; line-height:2.25rem">
                         {{ __("Freshly Baked Goodness, Loved by Filipinos. Franchise Now or Order Online!") }}
                     </div>
                     <div class="p-4 flex gap-4"> 
@@ -20,7 +20,7 @@
 
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="{{ asset('images/bread.png') }}" alt="Your Logo" class="h-24 w-auto">
+                        <img src="{{ asset('images/bread.png') }}" alt="Your bread" class="h-4/6 w-auto">
                     </a>
                 </div>
 
